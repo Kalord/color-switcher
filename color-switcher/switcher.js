f0b968a0025ed6b7c45244222788ee1c49db3ee6
Artem Tyutnev <artem.tyutnev.developer@gmail.com>
2020-01-14 20:39:58 +0800

    Added logic for working with non input elements

--- color-switcher/switcher.js
+++ color-switcher/switcher.js
@@ -2,13 +2,13 @@
  * Решение для динамического изменения цвета у HTML элементов
  * 
  * Данная библиотека предполагает, что источник цвета содержит
- * идентификатор "#source-{$className}". 
+ * класс "#source-{$className}". 
  * Где $className - это название класса у элементов, 
  * которые будут изменяться.
  * 
  * Пример, источник цвета определен как:
  *      ```html
- *      <input type="text" id="source-element">
+ *      <input type="text" class="source-element">
  *      ```
  * 
  * Тогда, элементы, которые должны изменяться, должны выглядить
@@ -34,8 +34,8 @@
  * Поиск HTML элемента источника цвета
  */
 const findIdSource = () => {
-    let id = document.body.innerHTML.match(/\id="source-\w+"/ig)[0];
-    id = id.replace(/id="/, "");
+    let id = document.body.innerHTML.match(/class="source-\w+"/ig)[0];
+    id = id.replace(/class="/, "");
     return id.substr(0, id.length - 1);
 };
 
@@ -46,7 +46,7 @@ const getSource = () => {
     let idSource;
     if (!(idSource = findIdSource())) return false;
 
-    return $(`#${idSource}`);
+    return $(`.${idSource}`);
 };
 
 /**
@@ -58,7 +58,7 @@ const getSwitchElements = () => {
 }
 
 const changeColor = (element, hexColor) => {
-    $(element).css('background', `#${hexColor}`);
+    $(element).css('background', `${hexColor}`);
 }
 
 /**
@@ -70,10 +70,15 @@ let sourceElement = getSource();
  */
 let switchElements = getSwitchElements();
 
-sourceElement.change((event) => {
-    hexColor = sourceElement.val();
+const eventHandler = (event) => {
+    hexColor = sourceElement.val() ? sourceElement.val() : sourceElement.html();
+
+    console.log(hexColor);
 
     switchElements.filter((element) => {
         changeColor(switchElements[element], hexColor);
-    })
-})
+    });
+}
+
+sourceElement.change(eventHandler);
+sourceElement.on('DOMSubtreeModified', eventHandler);
